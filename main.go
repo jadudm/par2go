338be86c6262cd20ac189852b7cc1a4b776b8ba8
Matt Jadud <matt@jadud.com>
2020-04-11 09:58:36 -0400

    Exploring PAR2 in Golang.
    This is a chance to explore encoding as well as Go.
    
    Tests need to be next.

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
-package par2go
+package main
 
-func Hello () string {
-	return "Hi!"
+import (
+	"fmt"
+	"os"
+)
+
+// checkInputFile makes sure the string passed on the
+// command line is a file, and a valid PAR2 file to boot.
+func checkInputFile(file string) {
+	CheckForFile(file)
+	CheckForPAR2Header(file)
+}
+
+func main() {
+	fmt.Println("par2go")
+
+	// Grab the arguments without the application name.
+	args := os.Args[1:]
+	// TODO: check how things come in. For now, assume one
+	// file on the command line.
+	file := args[0]
+	result, err := CheckForPAR2Header(file)
+	Check(err)
+	fmt.Println(result)
 }
